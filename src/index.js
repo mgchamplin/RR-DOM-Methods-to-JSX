8474ae351873a400a5272df7d30af486ed04236b
Michael Champlin <mchamplin@sbcglobal.net>
2022-04-16 11:12:48 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,38 +1,40 @@
-let koalaContainer = document.createElement('div')
-
-koalaContainer.setAttribute('class', 'ui items')
+let koalaContainer = <div className="ui items"></div>
 
 // Koala Card
 // koalas are in './public/koalas.js'
 koalas.forEach(koala => {
-    let koalaCard = document.createElement('div')
-    koalaCard.setAttribute('class', 'item')
-    koalaCard.style.cursor = 'pointer';
+    
+    //koalaCard.style.cursor = 'pointer';
+    koalaCard = <div className="item" style={{ cursor: 'pointer' }}></div>
 
     // Koala Card Image
-    let imageContainer = document.createElement('div')
-    let koalaImage = document.createElement('img')
 
-    imageContainer.setAttribute('class', 'image')
+    let imageContainer = <div className="image"></div>
 
-    koalaImage.setAttribute('src', koala.imageURL)
+    //let koalaImage = document.createElement('img')
+    //koalaImage.setAttribute('src', koala.imageURL)
+    //imageContainer.append(koalaImage)
+    let koalaImage = imageContainer.append(<img src={koala.imageURL}></img>)
 
-    imageContainer.append(koalaImage)
 
     // Koala Card Content
-    let koalaContent = document.createElement('div')
-    koalaContent.setAttribute('class', 'content')
+    //let koalaContent = document.createElement('div')
+    //koalaContent.setAttribute('class', 'content')
+    <div className="content"></div>
 
-    let header = document.createElement('div')
-    header.setAttribute('class', 'header')
-    header.append(koala.name)
+    //let header = document.createElement('div')
+    //header.setAttribute('class', 'header')
+    //header.append(koala.name)
+    let header = <div className="header">{koala.name}</div>
 
-    let descriptionParagraph = document.createElement('p')
-    descriptionParagraph.append(koala.description)
+    //let descriptionParagraph = document.createElement('p')
+    //descriptionParagraph.append(koala.description)
+    let descriptionParagraph = <p>{koala.description}</p> 
 
-    let descriptionContainer = document.createElement('div')
-    descriptionContainer.setAttribute('class', 'description')
-    descriptionContainer.append(descriptionParagraph)
+    //let descriptionContainer = document.createElement('div')
+    //descriptionContainer.setAttribute('class', 'description')
+    //descriptionContainer.append(descriptionParagraph)
+    let descriptionContainer = <div className="description">{descriptionParagraph}</div>
 
     koalaContent.append(
         header,
